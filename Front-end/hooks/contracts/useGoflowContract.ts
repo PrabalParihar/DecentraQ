352d1c0d080d6e4e2d737b75aecdb70047f0049a
Prabal <prabalparihar95@gmail.com>
2023-08-10 21:25:51 +0530

    token contract

---
+++ Front-end/hooks/contracts/useGoflowContract.ts
@@ -0,0 +1,51 @@
+import * as wagmi from 'wagmi';
+import { useProvider, useSigner } from 'wagmi';
+import { makeNum } from '../../lib/number-utils';
+import type { BigNumber } from 'ethers';
+import GoflowContract from '../../lib/contracts/Goflow.json';
+
+export enum TokenEvent {
+  Transfer = 'Transfer',
+  Mint = 'Mint',
+}
+
+export type Amount = BigNumber;
+
+const useGoflowContract = () => {
+  const { data: signer } = useSigner();
+  const provider = useProvider();
+
+  const contract = wagmi.useContract({
+    // Change this adress after every deploy!
+    addressOrName: '0xa7772D8D51Ed7b8955A93Cec2685FD7B01a66DBf',
+    contractInterface: GoflowContract.abi,
+    signerOrProvider: signer || provider,
+  });
+
+  // Get a user's balance and convert to readable number
+  const getBalance = async (address: string): Promise<string> => {
+    const userBalanceBN = await contract.balanceOf(address);
+    return makeNum(userBalanceBN);
+  };
+
+  // "We have to approve before we can move" with transferFrom
+  const approve = async (address: string, amount: BigNumber): Promise<void> => {
+    const tx = await contract.approve(address, amount);
+    await tx.wait();
+  };
+
+  const mint = async (amount: BigNumber): Promise<void> => {
+    const tx = await contract.mint(amount);
+    await tx.wait();
+  };
+
+  return {
+    contract,
+    chainId: contract.provider.network?.chainId,
+    mint,
+    approve,
+    getBalance,
+  };
+};
+
+export default useGoflowContract;
